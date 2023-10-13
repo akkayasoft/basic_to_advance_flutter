f36a3d5f583737b9bf641f3bbda9c3e24c0dec4f
akkayasoft <akkayasoft@gmail.com>
2023-10-13 06:26:53 +0300

    container ve sizedbox bitti

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '101/container_sized_box_learn.dart';
 import '101/text_learn.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: TextLearnView(),
+      home: ContainerSizedBoxLearn(),
     );
   }
 }
